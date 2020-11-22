1c93919bce74ed5d5fe4f3fb29f3068551960f17
Theodor <hennings.theodor@gmail.com>
2020-11-22 18:29:58 +0100

    stuff

--- MixJam/Assets/Scripts/Cards/Effects/CardMovement.cs
+++ MixJam/Assets/Scripts/Cards/Effects/CardMovement.cs
@@ -25,7 +25,7 @@ public class CardMovement : MonoBehaviour
 	private const float MEDIUMTIME = TIMEGOD * 0.35f;
 	private const float LONGTIME = TIMEGOD * 0.6f;
 
-	private const float cardRatio = 0.66f;
+	private const float cardRatio = 0.75f;
 
 	private bool isShaking;
 
